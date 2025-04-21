4a8801bd592ef5f4b0f311cc9d53a14085360e25
goutam19553 <51512424+goutam19553@users.noreply.github.com>
2025-04-21 13:57:49 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -43,7 +43,7 @@ const Navbar = () => {
         <div className="flex items-center justify-between">
           <div className="flex items-center">
             <Link to="/" className="flex items-center">
-              <span className="text-2xl font-heading font-bold text-Aura-white">
+              <span className="text-2xl font-heading font-bold text-Aura-navy">
                 Aura<span className="text-bharat-saffron">-Ad</span>
               </span>
             </Link>

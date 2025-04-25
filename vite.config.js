ede4af10ce8bebfe9386be78729b777044dcdb87
goutam19553 <51512424+goutam19553@users.noreply.github.com>
2025-04-25 08:27:06 +0530

    Update and rename vite.config.ts to vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,8 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { componentTagger } from "lovable-tagger";
+
 export default defineConfig(({ mode }) => ({
   base: "/",
   server: {
@@ -6,7 +11,7 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
-    mode === 'development' && componentTagger(),
+    mode === "development" && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {

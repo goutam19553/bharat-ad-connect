49216840ebef083f8c72d5221bac560087c7d366
goutam19553 <51512424+goutam19553@users.noreply.github.com>
2025-04-27 14:02:46 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
-import ARSolutions from "./pages/ARsolutions";  //
+import ARSolutions from "./pages/ARsolutions"; {/* Importing AR Solutions page */}
 import AdSpaces from "./pages/AdSpaces";
 import Advertisers from "./pages/Advertisers";
 import AdSpaceOwners from "./pages/AdSpaceOwners";

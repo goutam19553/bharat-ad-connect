c281c4844dedff88388f342248e03540e7d7a058
goutam19553 <51512424+goutam19553@users.noreply.github.com>
2025-04-29 18:16:17 +0530

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://your-project.supabase.co'; // replace with your URL
-const supabaseAnonKey = 'your-anon-key'; // replace with your key
+const supabaseUrl = 'https://eemwdgvywfjdvvhewglt.supabase.co'; // replace with your URL
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVlbXdkZ3Z5d2ZqZHZ2aGV3Z2x0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU5MjkzMzIsImV4cCI6MjA2MTUwNTMzMn0.WDZg5jLJo-XrbgaiX8A5OmamqyKk5Rdawthiku10faE'; // replace with your key
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);

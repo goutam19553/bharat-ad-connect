1f2fac16f4162870de36e91d2b445b54e7f124db
goutam19553 <51512424+goutam19553@users.noreply.github.com>
2025-05-01 14:16:04 +0530

    Update WallUpload.tsx

--- src/pages/WallUpload.tsx
+++ src/pages/WallUpload.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm, SubmitHandler } from "react-hookform";
+import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { toast } from "sonner";
 import { Button } from "@/components/ui/button";

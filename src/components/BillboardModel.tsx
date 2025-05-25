54e5625243563a2833669a1cc9b6cbb05bdfeed0
goutam19553 <51512424+goutam19553@users.noreply.github.com>
2025-05-25 21:32:59 +0530

    Update BillboardModel.tsx

--- src/components/BillboardModel.tsx
+++ src/components/BillboardModel.tsx
@@ -7,7 +7,7 @@ import { useCursor } from '@react-three/drei'
 import { useState } from 'react'
 
 function Billboard({ onClick }: { onClick: () => void }) {
-  const { scene } = useGLTF('/models/lowpoly_billboard.glb')
+  const { scene } = useGLTF('/low-poly_billboard.glb')
   const [hovered, setHovered] = useState(false)
   useCursor(hovered)
 

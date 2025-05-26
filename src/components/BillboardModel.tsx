74f5d5c6d74c6041ecb88989903f0b77a573ee9d
goutam19553 <51512424+goutam19553@users.noreply.github.com>
2025-05-26 12:27:20 +0530

    Update BillboardModel.tsx

--- src/components/BillboardModel.tsx
+++ src/components/BillboardModel.tsx
@@ -1,46 +1,29 @@
 'use client'
 
 import { Canvas } from '@react-three/fiber'
-import { OrbitControls, useGLTF, Stage } from '@react-three/drei'
-import { useCursor } from '@react-three/drei'
-import { useState } from 'react'
+import { useGLTF, Stage } from '@react-three/drei'
 
-function Billboard({ onClick }: { onClick: () => void }) {
+function Billboard() {
   const { scene } = useGLTF('/low-poly_billboard_pack.glb')
-  const [hovered, setHovered] = useState(false)
-  useCursor(hovered)
 
   return (
     <primitive
       object={scene}
       scale={1.5}
-      onPointerOver={() => setHovered(true)}
-      onPointerOut={() => setHovered(false)}
-      onClick={onClick}
     />
   )
 }
 
 export default function BillboardModel() {
-  const handleClick = () => {
-    alert('You clicked the billboard! 🚀')
-    // Or navigate to a booking page
-    // router.push('/book-wall-space')
-  }
-
- return (
-  <div className="w-full h-[800px] bg-gray-800">
-    <Canvas camera={{ position: [0, 3, 6], fov: 40 }}>
-      <ambientLight intensity={1} />
-      <Stage environment="city" intensity={0.6]}>
-        <Billboard onClick={handleClick} />
-      </Stage>
-      <OrbitControls 
-        enablePan={false} 
-        enableZoom 
-        enableRotate={false} 
-        target={[0, 1.5, 0]} // focus on middle of the billboard
-      />
-    </Canvas>
-  </div>
-)
+  return (
+    <div className="w-full h-[800px] bg-gray-800">
+      <Canvas camera={{ position: [0, 3, 6], fov: 40 }}>
+        <ambientLight intensity={1} />
+        <Stage environment="city" intensity={0.6}>
+          <Billboard />
+        </Stage>
+        {/* Removed OrbitControls to disable interaction */}
+      </Canvas>
+    </div>
+  )
+}

4df28a1d5d0453d342d5badb8911686b1368db52
goutam19553 <51512424+goutam19553@users.noreply.github.com>
2025-06-05 22:13:56 +0530

    Update PlexusBackground.tsx

--- src/components/PlexusBackground.tsx
+++ src/components/PlexusBackground.tsx
@@ -1,10 +1,7 @@
 
 const PlexusBackground = () => {
    return (
-    <div className="fixed top-0 left-0 w-full h-full z-[-1] overflow-hidden fancy-bg">
-      <div className="absolute inset-0 pointer-events-none fancy-overlay" />
-      <div className="absolute inset-0 pointer-events-none bg-particles" />
-    </div>
+    <div className="fixed top-0 left-0 w-full h-full z-[-1] aurora-gradient pointer-events-none" />
   );
 };
 export default PlexusBackground;

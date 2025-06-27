9b204eb90a6f72ca35012bd36223b351f6660c78
goutam19553 <51512424+goutam19553@users.noreply.github.com>
2025-06-27 19:28:24 +0530

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -63,17 +63,18 @@ const Index = () => {
       rating: 4,
       featured: true,
     },
-    {
-      id: 4,
-      title: "Prime Billboard - MG Road",
-      location: "MG Road",
-      city: "Bengaluru",
-      type: "Billboard",
-      size: "30 x 15 feet",
-      price: 45000,
-      image: "https://raw.githubusercontent.com/goutam19553/Startup-adtech/refs/heads/main/public/092bfbdd-50d2-411b-9914-d2734e65ebb3.jpg",
-      rating: 5,
-      featured: true,
+   {
+     id: 4,
+    title: "Water Bottle Ads",
+    location: "Anywhere",
+    city: "Anywhere",
+    type: "Product Branding",
+    size: "Reusable Bottle",
+    price: 5000,
+    image: "https://raw.githubusercontent.com/goutam19553/Startup-adtech/main/public/waterdemo.png", // ← Replace with your image later
+    rating: 4.7,
+    impressions: "High visibility in offices, gyms, and events",
+    featured: true
     },
     {
       id: 5,

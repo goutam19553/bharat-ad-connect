7f5cbff211117f3da40b7decd6c2267c0ba613bc
goutam19553 <51512424+goutam19553@users.noreply.github.com>
2025-06-28 19:11:31 +0530

    Create seo-sitemap.tsx

---
+++ src/pages/seo-sitemap.tsx
@@ -0,0 +1,16 @@
+export const prerender = true;
+
+export function GET() {
+  return new Response(`<?xml version="1.0" encoding="UTF-8"?>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
+      <loc>https://www.adproject.in/</loc>
+      <changefreq>weekly</changefreq>
+      <priority>1.0</priority>
+    </url>
+  </urlset>`, {
+    headers: {
+      'Content-Type': 'application/xml'
+    }
+  });
+}
